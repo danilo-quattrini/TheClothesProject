d830b3ee8e2209cb5029f46f3d70478d2dc7e5d3
DaniQ03 <daniloquattrini.com@gmail.com>
2024-07-11 21:42:34 +0200

    Just added the gradle wrapper and the main file
    +gradle wrapper
    +Main java file added to the project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
+import org.apache.jena.ontology.OntModel;
+import org.apache.jena.rdf.model.ModelFactory;
+
 public class Main {
+    static OntModel MyOntology = ModelFactory.createOntologyModel();
     public static void main(String[] args) {
+        MyOntology.read("../../../TheClothesProject.owx");
         System.out.println("Hello World!");
     }
 }
